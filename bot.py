624ecdbb986998d6b03969b4abea7ce4814d7fd0
chickenbay6-creator <chickenbay6@gmail.com>
2026-08-30 06:14:46 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,24 @@
 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  chno = "Bot is running!"
+  return chno
+
+
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
+
+# Make sure keep_alive() is called right before your bot runs
+keep_alive()
