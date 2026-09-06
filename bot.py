3933c9c5203a74f9b37b06b8a068f6b6acd79f16
chickenbay6-creator <chickenbay6@gmail.com>
2026-09-05 20:25:43 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ keep_alive()
 
 @tasks.loop(hours=1)
 async def hourly_spawn():
-    channel_id = 123456789012345678
+    channel_id = 123456789012345678  # Replace with your target channel ID
     channel = bot.get_channel(channel_id)
     if channel:
         await channel.send("A wild dex card has spawned! Use your commands to catch it.")
@@ -48,9 +48,9 @@ async def create_card(interaction: discord.Interaction):
 async def forcespawn(interaction: discord.Interaction):
     await interaction.response.defer(ephemeral=True)
     if interaction.user.id not in ADMIN_IDS:
-        await interaction.followup.send("You do not have permission to use this command.")
+        await interaction.followup.send("You do not have permission to use this command.", ephemeral=True)
         return
-    await interaction.followup.send("Spawn forced!")
+    await interaction.followup.send("Spawn forced!", ephemeral=True)
 
 @bot.event
 async def on_ready():
@@ -59,4 +59,4 @@ async def on_ready():
     if not hourly_spawn.is_running():
         hourly_spawn.start()
 
-bot.run("MTU0MzUxOTgwOTQzMjU5MjQxNQ.G8L54G.voOa0fdHz4uXQnmbqTjLPxaKJLILz-dygQ2V-M")
+bot.run("MTU0MzUxOTgwOTQzMjU5MjQxNQ.G-BF0B.sagFTjvKSx4jgtxVyMmMb5xUpjozvEa6mIpea0")
